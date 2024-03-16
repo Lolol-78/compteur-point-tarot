2d804f9290ff004ba73dd22807b107960001ef43
Lolol <loic.azavant@gmail.com>
2024-03-16 15:40:23 +0100

    truc

--- tarot.py
+++ tarot.py
@@ -7,3 +7,6 @@ from app.models import TarotGame, Player, Deal
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'TarotGame': TarotGame, 'Player': Player, 'Deal': Deal}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
